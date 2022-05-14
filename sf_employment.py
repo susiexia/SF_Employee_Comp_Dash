4eb37bbbc44e003782e9d4126e04f6f77b172406
Susie Xia <jiqianxia@gmail.com>
2022-05-14 01:41:53 -0700

    init a dash app

--- sf_employment.py
+++ sf_employment.py
@@ -3,16 +3,16 @@ import os
 import pandas as pd
 from sodapy import Socrata
 
-# dash lib
-import dash
+# dcc = dash_core_components 
+# from dash.dependencies import Input, Output, State
+
+from dash import Dash, html, dcc, Input, Output 
+
 import plotly.express as px
 import plotly.graph_objects as go
 
-import dash_core_components as dcc
-import dash_html_components as html
 
-
-app = dash.Dash(__name__)
+app = Dash(__name__)
 
 
 # import dataset, simple groupby for test
@@ -20,4 +20,30 @@ df = pd.read_csv('plotly_dash/data/Employee_Compensation.csv')
 
 test_df = df.groupby(['Year Type','Year','Job'])[['Salaries','Total Compensation']].mean()
 test_df.reset_index(inplace=True)
-print(test_df[:5])
+# print(test_df[:5])
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+
+# App layout
+app.layout = html.Div([
+    html.H1("SF Employment Job Dashboard", style={'text-align','center'}),
+    dcc.Dropdown(id="select_year_type", #id (string; optional): The ID of this component, used to identify dash components in callbacks. The ID needs to be unique across all of the components in an app.
+    options=[
+        {"label":"Calendar_type", "value":'Calendar'},
+        {"label":"Fiscal_type", "value":'Fiscal'}],
+        multi=False,
+        value='Calendar',
+        style={'width':"40%"}
+    ),
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='my_bee_map', figure={})
+])
+
+
+# execute py file
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    
